dcf0d7cb80f49158c5542926eed50cea3caf815b
Yuhao Xie <129975898+PSGLGDFaithbian7@users.noreply.github.com>
2024-02-04 00:07:46 +0800

    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

--- CsrIssue.v
+++ CsrIssue.v
@@ -194,12 +194,15 @@ cCopyFork2_32b cCopyFork02(
 
 //
 wire w_immediate_32;
+wire w_Rs1Valid_1;
+wire w_Rs2Valid_1;
 
 always @(*) begin
       w_dep1_4 = w_InsrtuctionOut_115[110:107];
-      w_dep2_4 = w_InsrtuctionOut_115[114:111];
       w_CsrInstruction_115 = w_InsrtuctionOut_115;
       w_immediate_32 = w_InsrtuctionOut_115[74:13];
+      w_Rs1Valid_1 = w_InsrtuctionOut_115[24];
+      w_Rs2Valid_1 = ~w_InsrtuctionOut_115[8];
 end
 
 output wire o_Drive_BypassPmtToBypass;
@@ -211,16 +214,16 @@ wire w_fireBypassPmt;
 wire w_fireGrfPmt;
 
 output wire [4:0] o_CsrRs1_5;
-output wire [4:0] o_CsrRs2_5;
+output wire [11:0] o_CsrRs2_12;
 
 reg [4:0] r_CsrRs1_5;
-reg [4:0] r_CsrRs2_5;
+reg [11:0] r_CsrRs2_12;
 
 reg [3:0] r_CsrDepL_4;
-reg [3:0] r_CsrDepR_4;
+
 
 output wire [3:0] o_CsrDepL_4;
-output wire [3:0] o_CsrDepR_4;
+
 
 cPmtFifo1 cPmtFifo_ToBypass(
         .i_drive(w_Drive_FromCsrLssueToGRFPmt_1),
@@ -247,7 +250,7 @@ assign o_CsrDepL_4 = r_CsrDepL_4;
 assign o_CsrDepR_4 = r_CsrDepR_4;
 
 
- cPmtFifo1 cPmtFifo_ToGRF(
+ cPmtFifo1 cPmtFifo_ToGRFandCsr(
         .i_drive(w_Drive_FromCsrLssueToBypassBufferPmt_1),
         .i_freeNext(i_Free_GrfToGrfPmt), 
         .rst(rstn),
@@ -260,16 +263,30 @@ assign o_CsrDepR_4 = r_CsrDepR_4;
 always @(posedge w_fireGrfPmt or negedge rstn ) begin
         if(!rstn) begin
         r_CsrRs1_5 <= 5'b0;
-        r_CsrRs2_5 <= 5'b0;
+        r_CsrRs2_12 <= 12'b0;
         end
         else begin
         r_CsrRs1_5 <= w_InsrtuctionOut_115[29:24];
-        r_CsrRs2_5 <= w_InsrtuctionOut_115[35:30];
+        r_CsrRs2_12 <= w_InsrtuctionOut_115[33:12];
         end
 end     
 
 assign o_CsrRs1_5 = r_CsrRs1_5;
-assign o_CsrRs2_5 = r_CsrRs2_5;
+assign o_CsrRs2_12 = r_CsrRs2_12;
+//改
+cCopyFork2_32b cCopyFork02(
+    i_drive(o_Drive_FromCopyFork0ToCopyFork02_1),
+    i_freeNext0(w_FreeFromGRFPmtToCsrIssue_1),
+    i_freeNext1(w_FreeFromBypassBufferPmtToCsrIssue_1),
+    rst(rstn),
+    i_data_32(w_CsrOutputData2_32),
+    o_free(w_Free_CopyFork02ToCopyFork0_1),
+    o_driveNext0(w_Drive_FromCsrLssueToGRFPmt_1),
+    o_driveNext1(w_Drive_FromCsrLssueToBypassBufferPmt_1),
+    o_data0(),
+    o_data1(),
+);
+
 //
 input wire [31:0] i_CsrOperandFromGrf_32;
 input wire [31:0] i_CsrOperandFromBypassBuffer_32;
@@ -283,25 +300,15 @@ wire   w_DriveGRFPmtToMutexMerge1L_1;
 wire   w_DriveBypassPmtToMutexMerge1L_1;
 
 
-wire   w_IsDriveWithDepR_1 = (w_dep2_4==4'b1111) ? 1'b0: 1'b1;
-wire   w_IsDriveWithGRFR_1=~(w_IsDriveWithDepR_1);
-wire   w_FreeMutexMerge1RToBypassPmt_1;
-wire   w_FreeMutexMerge1RToGRFPmt_1;
-wire   w_DriveGRFPmtToMutexMerge1R_1;
-wire   w_DriveBypassPmtToMutexMerge1R_1;
-
-wire w_FreeWaitMergeToMutexMergeR_1;
-wire w_DriveMutexMergeRToWaitMerge_1;
-
 input wire i_Drive_GRF_To_CsrIssueL_1;
-input wire i_Drive_GRF_To_CsrIssueR_1;
+input wire i_Drive_CSR_To_CsrIssueR_1;
 input wire i_Drive_BypassBuffer_To_CsrIssueL_1;
-input wire i_Drive_BypassBuffer_To_CsrIssueR_1;
+
 
 output wire o_Free_CsrIssueL_To_GRF_1;
-output wire o_Free_CsrIssueR_To_GRF_1;
+output wire o_Free_CsrIssueR_To_CSR_1;
 output wire o_Free_CsrIssueL_To_BypassBuffer_1;
-output wire o_Free_CsrIssueR_To_BypassBuffer_1;
+
 
 wire w_DriveWaitMergeToIssueFifo_1;
 wire w_FreeIssueFifoToWaitMerge_1;
@@ -346,45 +353,16 @@ cMutexMerge2_32b  cMutexMerge1L(
 );
 
 
+//改
 
-
- cPmtFifo1 cPmtFifo_FromBypassR(
-        .i_drive(i_Drive_BypassBuffer_To_CsrIssueR_1),
-        .i_freeNext(w_FreeMutexMerge1RToBypassPmt_1), 
+ cPmtFifo1 cPmtFifo_FromCsrR(
+        .i_drive(i_Drive_Csr_To_CsrIssueR_1),
+        .i_freeNext(w_FreeMutexMerge1RToCsrPmt_1), 
         .rst(rstn),
-        .pmt(w_IsDriveWithDepR_1),
-        .o_free(o_Free_CsrIssueL_To_BypassBuffer_1),
-        .o_driveNext(w_DriveBypassPmtToMutexMerge1R_1),
+        .pmt(w_IsDriveWithCsrR_1),
+        .o_free(o_Free_CsrIssueR_To_Csr_1),
+        .o_driveNext(w_DriveCsrPmtToMutexMerge1R_1),
         .o_fire_1()
-);
-
-
-
- cPmtFifo1 cPmtFifo_FromGRFR(
-        .i_drive(i_Drive_GRF_To_CsrIssueR_1),
-        .i_freeNext(w_FreeMutexMerge1RToGRFPmt_1), 
-        .rst(rstn),
-        .pmt(w_IsDriveWithGRFR_1),
-        .o_free(o_Free_CsrIssueR_To_GRF_1),
-        .o_driveNext(w_DriveGRFPmtToMutexMerge1R_1),
-        .o_fire_1()
-);
-
-
-
-
-
-cMutexMerge2_32b  cMutexMerge1R(
-         .i_drive0(w_DriveBypassPmtToMutexMerge1R_1),
-         .i_drive1(w_DriveGRFPmtToMutexMerge1R_1),
-         .i_data0_32(),
-         .i_data1_32(),
-         .i_freeNext(w_FreeWaitMergeToMutexMergeR_1),
-         .rst(rstn),
-         .o_free0(w_FreeMutexMerge1RToBypassPmt_1),
-         .o_free1(w_FreeMutexMerge1RToGRFPmt_1),
-         .o_driveNext(w_DriveMutexMergeRToWaitMerge_1),
-         .o_data_32(),
 );
 
 
@@ -418,7 +396,7 @@ cFifo1 IssueFifo(
 input wire [31:0] i_bypass_operandL_32;
 input wire [31:0] i_bypass_operandR_32;
 input wire [31:0] i_OeprandFromGrfL_32;
-input wire [31:0] i_OeprandFromGrfR_32;
+input wire [31:0] i_OeprandFromCsrR_32;
 
 wire [31:0]  w_final_oprandL_32;
 wire [31:0]  w_final_oprandR_32;
@@ -426,7 +404,7 @@ wire [31:0]  w_final_oprandR_32;
 
 
 assign w_final_oprandL_32 = (w_Rs1Valid_1==1'b1) ? w_immediate_32  : ((w_dep1_4==4'b1111)?  i_bypass_operandL_32 : i_OeprandFromGrfL_32);
-assign w_final_oprandR_32 = (w_Rs1Valid_1==1'b1) ? w_immediate_32  : ((w_dep2_4==4'b1111)?  i_bypass_operandR_32 : i_OeprandFromGrfR_32);
+assign w_final_oprandR_32 = (w_Rs2Valid_1==1'b1) ? w_immediate_32  :i_OeprandFromCsrR_32;
     
 reg  [31:0]  r_final_oprandL_32;
 reg  [31:0]  r_final_oprandR_32;
